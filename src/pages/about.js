19d60904d8dd3869460e8b6d3b2182352f37b759
Zac Cooper <zacooper5g@outlook.com>
2022-12-24 11:02:28 +1100

    added new custom pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>This is all about learning Gatsby</p>
+    </div>
+  )
+}
+
+export default About
