71abe84ee22b617ab22fcb4893d6d29d1ab26261
Zac Cooper <zacooper5g@outlook.com>
2022-12-26 10:46:32 +1100

    added home styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import * as React from "react";
+import * as styles from '../styles/home.module.css';
 import Layout from "../components/Layout";
+import { Link } from "gatsby";
 
 export default function Home() {
 	return (
 		<Layout>
-			<section>
+			<section className={ styles.header }>
 				<div>
-					<h1>Yay!</h1>
+					<h2 >Yay!</h2>
+					<h3>Having fun :)</h3>
+					<p>More yay</p>
+					<Link className={styles.btn} to="/projects" >My portfolio projects</Link>
 				</div>
 			</section>
 		</Layout>
