a3329388ee4823fb6d1750558e2366ac7399498f
Zac Cooper <zacooper5g@outlook.com>
2022-12-26 10:50:36 +1100

    added styles for projects page

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import * as styles from "../../styles/projects.module.css";
 
 const Projects = () => {
   return (
     <Layout>
-        <h1>Projects</h1>
+        <div className={styles.portfolio}>
+            <h1>Projects</h1>
+            <h3>Projects I've created</h3>
+        </div>
     </Layout>
   )
 }
