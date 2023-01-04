b28a1c2f5c90a161d7ddc7ce57dc1362df31721a
Zac Cooper <zacooper5g@outlook.com>
2023-01-04 14:18:29 +1100

    added projects styling

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -12,7 +12,7 @@ const Projects = ({ data }) => {
             <div className={styles.portfolio}>
                 <h1>Projects</h1>
                 <h3>Projects I've created</h3>
-                <div>
+                <div className={styles.projects} >
                     {projects.map(project => (
                         <Link to={"/projects/" + project.frontmatter.slug} key={project.id} >
                             <div>
