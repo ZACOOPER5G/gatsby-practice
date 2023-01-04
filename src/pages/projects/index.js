5b46e081e6a81aaf507fd7fcc591107ecef9003b
Zac Cooper <zacooper5g@outlook.com>
2023-01-04 12:40:27 +1100

    added project links

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,16 +1,46 @@
+import { graphql, Link } from 'gatsby';
 import React from 'react'
 import Layout from '../../components/Layout'
 import * as styles from "../../styles/projects.module.css";
 
-const Projects = () => {
-  return (
-    <Layout>
-        <div className={styles.portfolio}>
-            <h1>Projects</h1>
-            <h3>Projects I've created</h3>
-        </div>
-    </Layout>
-  )
+const Projects = ({ data }) => {
+    console.log(data)
+    const projects = data.allMarkdownRemark.nodes;
+
+    return (
+        <Layout>
+            <div className={styles.portfolio}>
+                <h1>Projects</h1>
+                <h3>Projects I've created</h3>
+                <div>
+                    {projects.map(project => (
+                        <Link to={"/projects/" + project.frontmatter.slug} key={project.id} >
+                            <div>
+                                <h3>{ project.frontmatter.title }</h3>
+                                <p>{ project.frontmatter.stack }</p>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+            </div>
+        </Layout>
+    )
 }
 
 export default Projects
+
+export const query = graphql`
+    query ProjectsPage {
+        allMarkdownRemark {
+        nodes {
+            html
+            frontmatter {
+            slug
+            stack
+            title
+            }
+            id
+        }
+        }
+    }
+`
