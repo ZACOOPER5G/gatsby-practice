42b5f38c80f4b98ece62201a61a6f42fc6977a3c
Zac Cooper <zacooper5g@outlook.com>
2023-01-05 14:57:54 +1100

    added dynamic data

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,20 +1,44 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import Img from 'gatsby-image';
+import * as styles from '../styles/project-details.module.css'
+import { graphql } from 'gatsby';
 
-const ProjectDetails = () => {
+const ProjectDetails = ({ data }) => {
+    const { html } = data.markdownRemark
+    const { title, stack, featured } = data.markdownRemark.frontmatter
+
     return (
         <Layout>
-            <div>
-                <h2>title</h2>
-                <h3>stack</h3>
-                <div>
-                    <Img fluid={''} />
+            <div className={ styles.details } >
+                <h2>{title}</h2>
+                <h3>{stack}</h3>
+                <div className={ styles.featured }>
+                    <Img fluid={ featured.childImageSharp.fluid } />
                 </div>
-                <div dangerouslySetInnerHTML={''} />
+                <div className={ styles.html } dangerouslySetInnerHTML={{__html: html}} />
             </div>
         </Layout>
     )
 }
 
-export default ProjectDetails 
+export default ProjectDetails
+
+export const query = graphql`
+    query ProjectDetaills($slug: String) {
+        markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+        html
+            frontmatter {
+                stack
+                title
+                featured {
+                    childImageSharp {
+                        fluid {
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
